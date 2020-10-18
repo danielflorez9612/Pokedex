4798cb32fa51bb91dbb0aa2f2f5434dd34726f6b
Daniel Florez <daniel.florez.contractor@gdevtools.com>
2020-10-18 12:29:43 -0500

    Initial Commit

---
+++ src/main/java/com/daniel/modyo/ModyoApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.modyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/modyo/ModyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.modyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
